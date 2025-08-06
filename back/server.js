5d4d099b9bcc356440468e10f680564580eaa3a5
Jonathan <jonathangager75@gmail.com>
2025-08-06 09:54:43 +0200

    .

--- back/server.js
+++ back/server.js
@@ -1,52 +1,36 @@
 import express from 'express';
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import documentsRouter from './routes/documentsRoutes.js';
+
+dotenv.config();
 
 const app = express();
-const port = 3000
+const port = 3000;
 
 app.use(express.json());
 
 const db = await mysql.createConnection({
-host: 'localhost',
-user: 'root',
-password: 'Charlene2025!',
-database: 'accessitheque',
+  host: 'localhost',
+  user: 'root',
+  password: 'Charlene2025!',
+  database: 'accessitheque',
 });
-
 
 app.get('/', async (req, res) => {
-try {
-const [rows] = await db.query("SHOW TABLES");
-res.json(rows);
-} catch (error) {
-res.status(500).json({ error: error.message });
-}
+  try {
+    const [rows] = await db.query('SHOW TABLES');
+    res.json(rows);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
+
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
-
-
-app.listen(port, () => {
-console.log(`serveur demarré sur http://localhost:${port}`);
-});
-import dotenv from 'dotenv';
-
-import documentsRouter from './routes/documentsRoutes.js'; 
-
-dotenv.config();
-
-
-
-app.use(express.json());
-
-//POur tester : A supp apres une fois que j'ai categorie et utilisateur
-// app.get('/', (req, res) => {
-//   res.send('API fonctionne !');
-// });
-
-
 app.use('/documents', documentsRouter);
 
 app.listen(port, () => {
