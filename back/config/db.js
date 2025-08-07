7df54cf06affef3e07a170174208e1214c2d05fb
Jonathan <jonathangager75@gmail.com>
2025-08-07 16:55:22 +0200

    .

--- back/config/db.js
+++ back/config/db.js
@@ -5,7 +5,7 @@ dotenv.config(); // Load environment variables
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD || "",
+  password: process.env.DB_PASS || "",
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
