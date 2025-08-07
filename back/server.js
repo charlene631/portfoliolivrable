6936fcc631f12b58eb919e71dc7bba5d5eb9db13
Charlene149611 <c.gausset911@gmail.com>
2025-08-07 17:20:46 +0200

    Sauvegarde temporaire avant changement de branche

--- back/server.js
+++ back/server.js
@@ -11,13 +11,12 @@ dotenv.config();
 
 const app = express();
 
-// ✅ Utilise le port fourni par Render OU 3000 en local
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-// ✅ Test route (retourne les tables MySQL)
+
 app.get('/', async (req, res) => {
   try {
     const [rows] = await pool.query("SHOW TABLES");
@@ -27,7 +26,7 @@ app.get('/', async (req, res) => {
   }
 });
 
-// ✅ Routes API
+
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/categories', categoryRouter);
